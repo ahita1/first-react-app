a1f46d7ae7a95739feb967680f1cde2c0928c663
ahita1 <ahitafani833@gmail.com>
2023-07-22 22:03:00 +0300

    Add New Feature

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+// PascalCasing
+function Message(){
+    // JSX JavaScript XML
+    const name = "Ahita"
+    if(name)
+        return <div>
+        <h1>Hey { name } how are you doing men</h1>
+    </div>
+    return <h1>hello everyone haha</h1>
+    
+}
+
+export default Message
